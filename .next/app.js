795eb3b59dbcf55eff1029d546f6c61f5916eb90
Michael <michael.bretagne@gmail.com>
2018-03-18 20:36:11 -0700

    Fix provider bug

--- .next/app.js
+++ .next/app.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "31242fbc89206b691efe"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "638e240a5dcb05fc43b9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","4":"bundles/pages/newcampaign.js"}[chunkId]||chunkId) + "-" + {"0":"cd525e4b59549ae01ab1","1":"dbedf8264eff5ac2eab4","2":"112a70b41bfbcd7c6dde","4":"35c1d5693b59cf36129b"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","5":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"cd525e4b59549ae01ab1","1":"dbedf8264eff5ac2eab4","2":"112a70b41bfbcd7c6dde","5":"34c7c0e50459b3ce6b82"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+// We are in the browser and metamask is running
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // We are on the server or the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "[YOUR INFURA ACCESS (https://infura.io/)]"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
